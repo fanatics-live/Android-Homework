9d42dd429b86bc26551f6b87c706f5cefd2f1ab2
dongminghe <dongming1.he@gmail.com>
2024-01-07 17:21:03 +0800

    add bottom sheet

--- app/src/main/java/com/lagunalabs/swapigraphql/ui/PeopleDetailScreen.kt
+++ app/src/main/java/com/lagunalabs/swapigraphql/ui/PeopleDetailScreen.kt
@@ -5,34 +5,39 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.ClickableText
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
-import com.lagunalabs.`swapi-graphql`.GetPeopleQuery
 
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.Icon
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.TextButton
-import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
+import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.withStyle
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import com.lagunalabs.swapigraphql.ui.bottomsheet.CustomBottomSheetContainer
 import com.lagunalabs.swapigraphql.ui.theme.MidnightBlue
+import com.lagunalabs.`swapi-graphql`.GetPeopleQuery
+import com.lagunalabs.swapigraphql.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -40,15 +45,16 @@ fun PeopleDetailScreen(person: GetPeopleQuery.Person, navController: NavControll
     val gradient = Brush.verticalGradient(
         colors = listOf(MidnightBlue, Color.Black)
     )
+    val paddingMedium = dimensionResource(id = R.dimen.padding_medium)
 
-    val showDialog = remember { mutableStateOf(false) }
+    val showButtonSheet = remember { mutableStateOf(false) }
 
     Box(
         modifier = Modifier
             .fillMaxSize()
             .background(brush = gradient)
     ) {
-        Column() {
+        Column {
             TopAppBar(
                 title = { Text(text = "Person") },
                 navigationIcon = {
@@ -56,56 +62,70 @@ fun PeopleDetailScreen(person: GetPeopleQuery.Person, navController: NavControll
                         Icon(Icons.Filled.ArrowBack, contentDescription = null)
                     }
                 },
-                colors = TopAppBarDefaults.smallTopAppBarColors(
+                colors = topAppBarColors(
                     containerColor = Color.Transparent,
-                    titleContentColor = Color.White,
-                    navigationIconContentColor = Color.White
+                    navigationIconContentColor = Color.White,
+                    titleContentColor = Color.White
                 )
             )
-
-            val annotatedString = buildAnnotatedString {
-                withStyle(style = SpanStyle(color = Color.White, fontSize = 26.sp)) {
-                    append("Click")
-                }
-                pushStyle(
-                    SpanStyle(
-                        color = Color.Blue,
-                        fontSize = 26.sp,
-                        textDecoration = TextDecoration.Underline
-                    )
-                )
-                append(" here ")
-                pop()
-                withStyle(style = SpanStyle(color = Color.White, fontSize = 26.sp)) {
-                    append("to view home data for ${person.name} ")
-                }
-            }
 
             ClickableText(
-                text = annotatedString,
-                onClick = { offset ->
-                    showDialog.value = true
+                text = annotatedString(person),
+                onClick = {
+                    showButtonSheet.value = true
                 },
-                modifier = Modifier.padding(16.dp)
+                modifier = Modifier.padding(paddingMedium)
             )
 
-            if (showDialog.value) {
-                AlertDialog(
-                    onDismissRequest = { showDialog.value = false },
-                    title = { Text(text = "Dialog Title") },
-                    text = { Text(text = "Dialog Description") },
-                    confirmButton = {
-                        TextButton(onClick = { showDialog.value = false }) {
-                            Text("OK")
-                        }
-                    }
-                )
+            if (showButtonSheet.value) {
+                buttonSheet(showButtonSheet)
             }
         }
     }
 }
 
-// according to stackoverflow answers, this is the way to 
-// implement a bottom sheet before compose material3 bottom sheet final released
-//https://stackoverflow.com/questions/72518262/how-to-implement-bottomsheet-in-material-3-jetpack-compose-android
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+private fun buttonSheet(showButtonSheet: MutableState<Boolean>) {
+    val paddingMedium = dimensionResource(id = R.dimen.padding_medium)
+    ModalBottomSheet(
+        modifier = Modifier,
+        sheetState = rememberModalBottomSheetState(
+            skipPartiallyExpanded = false
+        ),
+        dragHandle = null,
+        onDismissRequest = {
+            showButtonSheet.value = false
+        },
+        shape = RoundedCornerShape(
+            topStart = paddingMedium,
+            topEnd = paddingMedium
+        ),
+    ) {
+        // seems network call is not return homepage data, so I just hardcode the url
+        CustomBottomSheetContainer("https://swapi.dev/api/planets/1/")
+    }
+}
+
+@Composable
+private fun annotatedString(person: GetPeopleQuery.Person): AnnotatedString {
+    val annotatedString = buildAnnotatedString {
+        withStyle(style = SpanStyle(color = Color.White, fontSize = MaterialTheme.typography.headlineSmall.fontSize)) {
+            append("Click")
+        }
+        pushStyle(
+            SpanStyle(
+                color = Color.Blue,
+                fontSize = MaterialTheme.typography.headlineSmall.fontSize,
+                textDecoration = TextDecoration.Underline
+            )
+        )
+        append(" here ")
+        pop()
+        withStyle(style = SpanStyle(color = Color.White, fontSize = MaterialTheme.typography.headlineSmall.fontSize)) {
+            append("to view homeworld data for ${person.name} ")
+        }
+    }
+    return annotatedString
+}
 
--- app/src/main/java/com/lagunalabs/swapigraphql/ui/PeopleListScreen.kt
+++ app/src/main/java/com/lagunalabs/swapigraphql/ui/PeopleListScreen.kt
@@ -2,6 +2,7 @@ package com.lagunalabs.swapigraphql.ui
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,6 +11,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -20,10 +22,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.res.dimensionResource
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import com.lagunalabs.`swapi-graphql`.GetPeopleQuery
+import com.lagunalabs.swapigraphql.R
 import com.lagunalabs.swapigraphql.ui.theme.MidnightBlue
 
 @Composable
@@ -34,62 +37,79 @@ fun PeopleListScreen(
     val gradient = Brush.verticalGradient(
         colors = listOf(MidnightBlue, Color.Black)
     )
+    val paddingMedium = dimensionResource(id = R.dimen.padding_medium)
+
     Surface(
         modifier = Modifier
             .fillMaxSize()
             .background(brush = gradient),
     ) {
         Column(Modifier.background(brush = gradient)) {
-            Row(){
+            Row() {
                 Text(
                     text = "People",
                     fontSize = MaterialTheme.typography.headlineLarge.fontSize,
-                    modifier = Modifier.padding(16.dp, 16.dp, 16.dp, 16.dp),
+                    modifier = Modifier.padding(paddingMedium),
                     color = Color.White
                 )
             }
             Divider(color = Color.Gray)
-            PersonItem(persons = persons, navController = navController)
+
+            PersonList(persons = persons, navController = navController)
         }
     }
 }
 
 @Composable
-fun PersonItem(persons: List<GetPeopleQuery.Person>, navController: NavController) {
-    LazyColumn(modifier = Modifier
-        .fillMaxSize()
-        .background(color = Color.Transparent)) 
-            {
-        itemsIndexed(persons) { index, person ->
-            Row(modifier = Modifier
+fun PersonList(persons: List<GetPeopleQuery.Person>, navController: NavController) {
+    val paddingMedium = dimensionResource(id = R.dimen.padding_medium)
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        if (persons.isEmpty()) {
+            CircularProgressIndicator(
+                modifier = Modifier.align(Alignment.Center),
+                color = Color.White
+            )
+        }
+
+        LazyColumn(
+            modifier = Modifier
                 .fillMaxSize()
                 .background(color = Color.Transparent)
-                .padding(16.dp, 16.dp, 16.dp, 16.dp)
-                .clickable {
-                    navController.navigate("peopleDetail/$index")
-                }) {
-                Column(modifier = Modifier.weight(1f)) {
-                    Text(text = "Name: ${person.name}", color = Color.White)
-                    Text(
-                        text = "Height: ${person.height?.toString() ?: "Unknown"}",
-                        color = Color.White
-                    )
-                    Text(
-                        text = "Mass: ${person.mass?.toString() ?: "Unknown"}",
-                        color = Color.White
+        ) {
+            itemsIndexed(persons) { index, person ->
+                Row(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(color = Color.Transparent)
+                        .padding(paddingMedium)
+                        .clickable {
+                            navController.navigate("peopleDetail/$index")
+                        }
+                ) {
+                    Column(modifier = Modifier.weight(1f)) {
+                        Text(text = "Name: ${person.name}", color = Color.White)
+                        Text(
+                            text = "Height: ${person.height?.toString() ?: "Unknown"}",
+                            color = Color.White
+                        )
+                        Text(
+                            text = "Mass: ${person.mass?.toString() ?: "Unknown"}",
+                            color = Color.White
+                        )
+                    }
+
+                    Icon(
+                        Icons.Default.ArrowForward,
+                        contentDescription = null,
+                        modifier = Modifier.align(
+                            Alignment.CenterVertically
+                        ),
+                        tint = Color.White
                     )
                 }
-
-                Icon(
-                    Icons.Default.ArrowForward,
-                    contentDescription = null,
-                    modifier = Modifier.align(
-                        Alignment.CenterVertically
-                    ),
-                    tint = Color.White
-                )
+                Divider(color = Color.Gray)
             }
-            Divider(color = Color.Gray)
         }
     }
 }
