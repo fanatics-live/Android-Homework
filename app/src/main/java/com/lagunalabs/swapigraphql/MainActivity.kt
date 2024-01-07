be99e974243e0b0399b1426f7240cdf62e5c86f2
dongminghe <dongming1.he@gmail.com>
2024-01-08 00:10:03 +0800

    add view model to handle fetch data

--- app/src/main/java/com/lagunalabs/swapigraphql/MainActivity.kt
+++ app/src/main/java/com/lagunalabs/swapigraphql/MainActivity.kt
@@ -3,64 +3,48 @@ package com.lagunalabs.swapigraphql
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.core.tween
-import androidx.compose.animation.slideInHorizontally
-import androidx.compose.animation.slideOutHorizontally
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
-import androidx.navigation.NavHostController
-import com.lagunalabs.`swapi-graphql`.GetPeopleQuery
-import com.lagunalabs.swapigraphql.networking.ApolloNetworking
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.lagunalabs.swapigraphql.ui.theme.SWAPIGraphQLTheme
-import kotlinx.coroutines.launch
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import com.lagunalabs.swapigraphql.ui.PeopleDetailScreen
 import androidx.navigation.compose.rememberNavController
-import com.lagunalabs.swapigraphql.ui.PeopleListScreen
+import com.lagunalabs.swapigraphql.ui.theme.Black
+import com.lagunalabs.swapigraphql.ui.theme.MidnightBlue
 
 class MainActivity : ComponentActivity() {
-    companion object {
-        private val networking by lazy { ApolloNetworking() }
-        const val TAG = "MainActivity"
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        
+
         // Remove the flash that appears during page switching
         window.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        val gradient = Brush.verticalGradient(
+            colors = listOf(MidnightBlue, Black)
+        )
+
         setContent {
             SWAPIGraphQLTheme {
-                val scope = rememberCoroutineScope()
-                val navController = rememberNavController()
-                var persons by remember { mutableStateOf(listOf<GetPeopleQuery.Person>()) }
-
-                LaunchedEffect(Unit) {
-                    scope.launch {
-                        val response = runCatching {
-                            networking.fetch(GetPeopleQuery())
-                        }
-
-                        response.onFailure {
-                            Log.e(TAG, it.message ?: it.toString())
-                        }
-
-                        response.onSuccess {
-                            persons = it.allPeople?.people?.filterNotNull() ?: listOf()
-                        }
+                val viewModel: MainViewModel = viewModel()
+                if (viewModel.errorMessage.value.isNotEmpty()) {
+                    // Show error message
+                    Box(modifier = Modifier
+                        .fillMaxSize()
+                        .background(brush = gradient)) {
+                        Text(text = viewModel.errorMessage.value)
                     }
+                } else {
+                    // Show content
+                    MainNavHost(
+                        navController = rememberNavController(),
+                        persons = viewModel.persons.value
+                    )
                 }
-
-                MainNavHost(navController = navController, persons)
             }
         }
     }
